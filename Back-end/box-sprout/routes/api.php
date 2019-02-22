ac38a657ccd5a4d1e723450f57a1cde442b809c9
fagenorn <18429405+fagenorn@users.noreply.github.com>
2019-02-22 10:03:41 +0100

    Route update (#5)
    * Removed user route

--- Back-end/box-sprout/routes/api.php
+++ Back-end/box-sprout/routes/api.php
@@ -2,9 +2,10 @@
 
 use Illuminate\Http\Request;
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
 
 // public routes
 Route::post('/login', 'Api\AuthController@login')->name('login.api');
